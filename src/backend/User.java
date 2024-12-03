86f65340b81b730f3a7312dfec5c468fe585e11d
Youssef <YoussefSsalah1605@gmail.com>
2024-12-03 23:39:52 +0200

    adding setters and getters to User.class

--- src/backend/User.java
+++ src/backend/User.java
@@ -12,20 +12,19 @@ import java.util.Date;
  */
 public class User {
     private static int userIdCounter = 0;
-    private String userId;
-    private String email;
-    private String username;
+    private final String userId;
+    private final String email;
+    private final String username;
     private String hashedPassword;
-    private Date dateOfBirth;
+    private final Date dateOfBirth;
     private String status;
     private String profilePhotoPath;
     private String coverPhotoPath;
     private String bio;
-    
-    
+
+    // Constructor ... ADD: could add later builder design pattern
     public User(String email, String username, String hashedPassword, Date dateOfBirth) {
-        
-        this.userId = "user"+ ++userIdCounter;
+        this.userId = "user" + ++userIdCounter;
         this.email = email;
         this.username = username;
         this.hashedPassword = hashedPassword;
@@ -35,8 +34,43 @@ public class User {
         this.coverPhotoPath = "none";
         this.bio = "none";
     }
-    
-    
-    
-    
+
+    // Getters
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    // Setters
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setProfilePhotoPath(String profilePhotoPath) {
+        this.profilePhotoPath = profilePhotoPath;
+    }
+
+    public void setCoverPhotoPath(String coverPhotoPath) {
+        this.coverPhotoPath = coverPhotoPath;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
+
