4a66e20599419365d4ce977f2689c082819fa19b
Helmy-434 <youssefhelmy1113@gmail.com>
2024-12-03 18:19:42 +0200

    First Steps with some logic and links with different frames but still missing implementation of many features but no errors yet

---
+++ src/backend/Post.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+import java.awt.image.BufferedImage;
+import java.util.Date;
+
+/**
+ *
+ * @author BLU-RAY
+ */
+public class Post extends Content{
+
+    public Post(String content, String contentid, String authorid, Date timestamp, BufferedImage image) {
+        this.content = content;
+        this.contentid = contentid;
+        this.authorid = authorid;
+        this.timestamp = timestamp;
+        this.image = image;
+    }
+
+    public Post(String content, String contentid, String authorid, Date timestamp) { //two constructors one for w img and other wo img
+        this.content = content;
+        this.contentid = contentid;
+        this.authorid = authorid;
+        this.timestamp = timestamp;
+        this.image = null; //if no img selected 
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getContentid() {
+        return contentid;
+    }
+
+    public String getAuthorid() {
+        return authorid;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+    
+    @Override
+    public void storeContent(){
+        
+    }
+    
+}
---
+++ src/backend/Story.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+import java.awt.image.BufferedImage;
+import java.util.*;
+import java.time.*;
+
+/**
+ *
+ * @author BLU-RAY
+ */
+public class Story extends Content {
+
+    public Story(String content, String contentid, String authorid, Date timestamp, BufferedImage image) {
+        this.content = content;
+        this.contentid = contentid;
+        this.authorid = authorid;
+        this.timestamp = timestamp;
+        this.image = image;
+    }
+
+    public Story(String content, String contentid, String authorid, Date timestamp) { //two constructors one for w/ img and other w/o img
+        this.content = content;
+        this.contentid = contentid;
+        this.authorid = authorid;
+        this.timestamp = timestamp;
+        this.image = null; //if no img selected 
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getContentid() {
+        return contentid;
+    }
+
+    public String getAuthorid() {
+        return authorid;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    @Override
+    public void storeContent() {
+        LocalDate today = LocalDate.now();
+        LocalDate time = this.timestamp.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); // convert to localdate bec duration doesnt work with date
+        Duration duration = Duration.between(time, today);
+        if(duration.toHours()>= 24)
+            return ;
+        else{
+        
+        }
+    }
+}
