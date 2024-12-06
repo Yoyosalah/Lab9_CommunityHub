435c4703500a58b22a4418b84c4f731abf1614fd
mostafaElKassar <mostafakassar@icloud.com>
2024-12-06 16:25:59 +0200

    added render posts

--- src/frontend/AddPost.java
+++ src/frontend/AddPost.java
@@ -154,6 +154,7 @@ public class AddPost extends javax.swing.JFrame {
             );
             this.setVisible(false);
         }
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
