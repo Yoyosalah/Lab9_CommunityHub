4f1876491ce109c13c123a45b43282bfa4a96da5
Helmy-434 <youssefhelmy1113@gmail.com>
2024-12-13 23:08:50 +0200

    Final Update

--- src/frontend/NotificationsPanel.java
+++ src/frontend/NotificationsPanel.java
@@ -95,7 +95,7 @@ public class NotificationsPanel extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 495, Short.MAX_VALUE)
+            .addGap(0, 688, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
